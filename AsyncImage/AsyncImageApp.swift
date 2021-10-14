ff344608b096ce6afdd3d1994aa72559c30c7a67
Luka <vujnovacluka89@gmail.com>
2021-10-14 20:52:47 +0200

    Initial Commit

---
+++ AsyncImage/AsyncImageApp.swift
@@ -0,0 +1,17 @@
+//
+//  AsyncImageApp.swift
+//  AsyncImage
+//
+//  Created by Luka Vujnovac on 14.10.2021..
+//
+
+import SwiftUI
+
+@main
+struct AsyncImageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
